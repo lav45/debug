1b7b00deaef90eb2ab4629cb93a9a806222562b1
Alex <LAV451@gmail.com>
2019-07-26 14:09:48 +0300

    Update debug.php

--- debug.php
+++ debug.php
@@ -30,10 +30,12 @@ function debug()
         $func = is_scalar($res) || null === $res ? 'var_export' : 'print_r';
         $res = $func($res, true);
     }
-    $isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
-        $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest' &&
-        $_SERVER['HTTP_CONTENT_TYPE'] === 'application/json';
+    $isAjax =
+        (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest') &&
+        (isset($_SERVER['HTTP_CONTENT_TYPE']) && $_SERVER['HTTP_CONTENT_TYPE'] === 'application/json');
+
     $isHtml = isset($_SERVER['HTTP_ACCEPT']) && strpos($_SERVER['HTTP_ACCEPT'], 'text/html') !== false;
+
     if ($isHtml && !$isAjax) {
         $res = '<pre>' . htmlspecialchars($res, ENT_IGNORE) . '</pre>';
     }
